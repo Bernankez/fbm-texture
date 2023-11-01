c9e4cbee894ee5ae2d91050ddc889fa9f7302db3
cole <bernankez@qq.com>
2023-11-02 02:09:33 +0800

    chore: fix lint

--- playground/.eslintrc.cjs
+++ playground/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "**/*.md"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "**/*.md", "tsconfig.*", "package.json"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
